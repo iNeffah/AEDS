ded936320873dadaf9c15c2847f3957c0768ca9b
João Neffa <jpsousa837@gmail.com>
2022-08-09 22:06:03 -0300

    Aula Busca em Grafos 1.1

--- Aula_Grafos/Busca_Grafos/exercicios.c
+++ Aula_Grafos/Busca_Grafos/exercicios.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include "grafos.h"
 
+// Cores branco cinza e preto do vertice são definidos como 0 1 e 2
+#define w 0
+#define g 1
+#define b 2
+
 typedef struct info_BL {
-    int *d;
-    int *p;
-    char *colors;
-    FilaL *queue;
+    int *d; // distancias
+    int *p; // predecessores
+    int *colors; // cores
+    FilaL *queue; // Fila para adjacentes
 } Info_BL;
 
-void BFS(Lista **grafo, int origem) {
-    
+void BFS(Lista **grafo, int origem, int n_vertices) {
+    Info_BL info;
+    info.d = (int *) malloc(n_vertices * sizeof(int));
+    info.p = (int *) malloc(n_vertices * sizeof(int));
+    info.colors = (int *) malloc(n_vertices * sizeof(int));
+    info.queue = fila_cria_l();
+
 }
